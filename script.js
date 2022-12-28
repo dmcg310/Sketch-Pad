c0fdff8b68c70f6ef4ad283e0002a89c90689890
dmcg310 <mcgurk-d5@ulster.ac.uk>
2022-12-28 14:30:32 +0000

    Added basic 'hover' effect

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
+
 for (i = 0; i < 256; i++) {
   const div = document.createElement("div");
   div.setAttribute("style", "border: 1px solid black; padding: 10px;");
-
   container.appendChild(div);
 }
