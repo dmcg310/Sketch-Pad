63529c990ee2763a4d7744586c985e1f063d8e6f
dmcg310 <mcgurk-d5@ulster.ac.uk>
2022-12-28 15:37:07 +0000

    More CSS, drawing pad looks nicer

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const container = document.querySelector("#container");
 
 for (i = 0; i < 256; i++) {
   const div = document.createElement("div");
-  div.setAttribute(
-    "style",
-    "border: 1px solid black; border-radius: 3px; padding: 15px;"
-  );
+  div.setAttribute("style", "border-radius: 3px; padding: 15px;");
   container.appendChild(div);
   div.addEventListener("mouseover", (e) => e.target.classList.add("my-color-class"));
 }
