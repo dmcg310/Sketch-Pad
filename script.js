eb27e54fd74547cbe29e880f81d11db181101ee1
dmcg310 <mcgurk-d5@ulster.ac.uk>
2022-12-28 14:51:02 +0000

    Improved hover effect

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const container = document.querySelector("#container");
 
 for (i = 0; i < 256; i++) {
   const div = document.createElement("div");
-  div.setAttribute("style", "border: 1px solid black; padding: 10px;");
+  div.setAttribute(
+    "style",
+    "border: 1px solid black; border-radius: 3px; padding: 15px;"
+  );
   container.appendChild(div);
+  div.addEventListener("mouseover", (e) => e.target.classList.add("my-color-class"));
 }
