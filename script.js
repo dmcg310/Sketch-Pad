c89a4133702ec518dcaf373a1991699ff80a44dd
dmcg310 <mcgurk-d5@ulster.ac.uk>
2022-12-31 17:38:16 +0000

    Added 7 colour options with a reset button and eraser

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// let defaultSize = prompt("Choose");
-let defaultSize = 16;
+let defaultSize = prompt(
+  "Choose a size for the sketch pad (max 100).\nPlease note that the larger the number the longer the page will take to load."
+);
+
 const container = document.querySelector("#grid-container");
 const grid = document.getElementById("grid-container");
 
@@ -8,116 +10,92 @@ grid.style.gridTemplateRows = `repeat(${defaultSize}, 1fr)`;
 grid.style.width = "500px";
 grid.style.height = "500px";
 grid.style.border = "#f95959 5px solid";
-grid.style.backgroundColor = "#e3e3e3";
+grid.style.backgroundColor = "white";
 
-for (let i = 0; i < defaultSize * defaultSize; i++) {
-  const gridElement = document.createElement("div");
-  gridElement.style.border = "0.75px solid black";
-  gridElement.classList.add("grid-element");
-  grid.appendChild(gridElement);
+if (defaultSize > 0 && defaultSize <= 100) {
+  for (let i = 0; i < defaultSize * defaultSize; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+    grid.appendChild(gridElement);
 
-  const randomR = Math.floor(Math.random() * 256);
-  const randomG = Math.floor(Math.random() * 256);
-  const randomB = Math.floor(Math.random() * 256);
+    const R = Math.floor(Math.random() * 256);
+    const G = Math.floor(Math.random() * 256);
+    const B = Math.floor(Math.random() * 256);
 
-  gridElement.addEventListener(
-    "mouseover",
-    (e) => (e.target.style.backgroundColor = "black")
-  );
-
-  let rainbow = document.getElementById("rainbow");
-  rainbow.addEventListener("click", (event) => {
     gridElement.addEventListener(
       "mouseover",
-      (e) => (e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`)
+      (e) => (e.target.style.backgroundColor = "black")
     );
-  });
 
-  let blue = document.getElementById("blue");
-  blue.addEventListener("click", (event) => {
-    gridElement.addEventListener(
-      "mouseover",
-      (e) => (e.target.style.backgroundColor = "blue")
-    );
-  });
+    let rainbow = document.getElementById("rainbow");
+    rainbow.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`)
+      );
+    });
+
+    let red = document.getElementById("red");
+    red.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "red")
+      );
+    });
+
+    let orange = document.getElementById("orange");
+    orange.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "orange")
+      );
+    });
+
+    let yellow = document.getElementById("yellow");
+    yellow.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "yellow")
+      );
+    });
+
+    let green = document.getElementById("green");
+    green.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "green")
+      );
+    });
+
+    let blue = document.getElementById("blue");
+    blue.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "blue")
+      );
+    });
+
+    let purple = document.getElementById("purple");
+    purple.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "purple")
+      );
+    });
+
+    let eraser = document.getElementById("eraser");
+    eraser.addEventListener("click", (event) => {
+      gridElement.addEventListener(
+        "mouseover",
+        (e) => (e.target.style.backgroundColor = "white")
+      );
+    });
+
+    let reset = document.getElementById("reset");
+    reset.addEventListener("click", (event) => {
+      gridElement.style.backgroundColor = "white";
+    });
+  }
+} else {
+  alert("Please enter a value that is greater than 0 and less than 100.");
 }
-
-// let rainbow = document.getElementById("rainbow");
-// rainbow.addEventListener("click", (event) => {
-//   gridElement.addEventListener(
-//     "mouseover",
-//     (e) => (e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`)
-//   );
-// });
-
-// let four = document.getElementById("four");
-// four.addEventListener("click", (event) => {
-//   fourGrid();
-// });
-
-// let eight = document.getElementById("eight");
-// eight.addEventListener("click", (event) => {
-//   eightGrid();
-// });
-
-// let sixteen = document.getElementById("sixteen");
-// sixteen.addEventListener("click", (event) => {
-//   sixteenGrid();
-// });
-
-// function fourGrid() {
-//   for (i = 0; i < 16; i++) {
-//     const div = document.createElement("div");
-//     div.setAttribute("style", "padding: 60px;");
-//     container.appendChild(div);
-//     const randomR = Math.floor(Math.random() * 256);
-//     const randomG = Math.floor(Math.random() * 256);
-//     const randomB = Math.floor(Math.random() * 256);
-//     div.addEventListener(
-//       "mouseover",
-//       (e) => (e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`)
-//     );
-//     let reset = document.getElementById("reset");
-//     reset.addEventListener("click", (event) => {
-//       container.removeChild(div);
-//     });
-//   }
-// }
-
-// function eightGrid() {
-//   for (i = 0; i < 64; i++) {
-//     const div = document.createElement("div");
-//     div.setAttribute("style", "padding: 30px;");
-//     container.appendChild(div);
-//     const randomR = Math.floor(Math.random() * 256);
-//     const randomG = Math.floor(Math.random() * 256);
-//     const randomB = Math.floor(Math.random() * 256);
-//     div.addEventListener(
-//       "mouseover",
-//       (e) => (e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`)
-//     );
-//     let reset = document.getElementById("reset");
-//     reset.addEventListener("click", (event) => {
-//       container.removeChild(div);
-//     });
-//   }
-// }
-
-// function sixteenGrid() {
-//   for (i = 0; i < 256; i++) {
-//     const div = document.createElement("div");
-//     div.setAttribute("style", "padding: 15px;");
-//     container.appendChild(div);
-//     const randomR = Math.floor(Math.random() * 256);
-//     const randomG = Math.floor(Math.random() * 256);
-//     const randomB = Math.floor(Math.random() * 256);
-//     div.addEventListener(
-//       "mouseover",
-//       (e) => (e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`)
-//     );
-//     let reset = document.getElementById("reset");
-//     reset.addEventListener("click", (event) => {
-//       container.removeChild(div);
-//     });
-//   }
-// }
